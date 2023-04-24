41607602a1427e686d8093531c6a316368fe9a9c
Inaki Villar <inaki.seri@gmail.com>
2023-04-23 17:45:41 -0700

    update Gradle 8.1.1

--- src/test/kotlin/io/github/cdsap/testprocess/IntegrationTest.kt
+++ src/test/kotlin/io/github/cdsap/testprocess/IntegrationTest.kt
@@ -46,7 +46,7 @@ class InfoTestProcessPluginTest {
                 }
             """.trimIndent()
         )
-        listOf("7.5.1", "7.6", "8.0.1").forEach {
+        listOf("7.5.1", "7.6", "8.0.1","8.1.1").forEach {
             val firstBuild = GradleRunner.create()
                 .withProjectDir(testProjectDir.root)
                 .withArguments("test", "--configuration-cache")
